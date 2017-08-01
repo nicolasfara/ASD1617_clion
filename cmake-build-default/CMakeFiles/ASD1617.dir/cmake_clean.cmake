7c8055e422cb75d19432a7b04ad67ceea4d1b48c
nicolasfara <nicolas.farabegoli@gmail.com>
2017-08-01 09:43:46 +0200

    firstcommit

---
+++ cmake-build-default/CMakeFiles/ASD1617.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ASD1617.dir/main.c.o"
+  "CMakeFiles/ASD1617.dir/lib1617.c.o"
+  "ASD1617.pdb"
+  "ASD1617"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ASD1617.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
